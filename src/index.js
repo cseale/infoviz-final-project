c500c5d2c6c639f43b0cfbbdd1456f4cc0fdab01
colm_seale <colm.seale@gmail.com>
2018-11-26 12:47:00 +0100

    added datamaps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+import d3 from 'd3';
+import 'topojson';
+import Datamap from 'datamaps';
+
+
+var map = new Datamap({
+    element: document.getElementById('container'),
+});
