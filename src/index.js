f4bbc3d478abeecb7781a836df458062870c161d
colm_seale <colm.seale@gmail.com>
2018-11-26 13:25:54 +0100

    added maps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+
+
 import d3 from 'd3';
 import 'topojson';
+
+// bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Datamap from 'datamaps';
 
 
 var map = new Datamap({
-    element: document.getElementById('container'),
+    element: document.getElementById('map'),
 });
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// webpack v4
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  }
+};
